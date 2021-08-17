c6491f0d0ab47f4d341cab6953e4d04e147dd193
ds.kim <ds.kim@wemakeprice.com>
2021-08-18 00:31:22 +0900

    3장 jpa setting

---
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,32 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import com.jojoldu.book.springboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+
+    public Posts toEntity(){
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
